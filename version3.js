d5ee1df7bd1ebd6c1615b9976c41296db3301612
Alex Bykov <spbeat@mail.ru>
2017-10-21 21:15:41 +0300

    forks

--- version3.js
+++ version3.js
@@ -14,7 +14,6 @@ const upperCommit = log[0].rev;
 svg.style.height = log.length * COMMIT_SPAN + 100 + 'px';
 
 log.forEach(e => {
-    //normalize object to store as {branch: [commits]}
     branches[e.branch]
         ? branches[e.branch].push(e)
         : (branches[e.branch] = [e]);
@@ -22,20 +21,51 @@ log.forEach(e => {
 
 console.log(branches);
 
-const sorts = log
-    .map(findRevisionSpread)
-    .filter((b, i) => log.findIndex(e => b.branch === e.branch) === i);
+const sorts = log.map(revisionSpread).filter(byUniqueBranch);
 
 sorts.forEach(createLine);
 
+Object.values(branches).forEach(createForks);
+
 console.log(sorts);
 
-function findRevisionSpread(b) {
+function createForks(branch) {
+    const parent = findByNodeId(branch.slice(-1)[0].parents[0]);
+    const self = findByNodeId(branch.slice(-1)[0].node);
+    connectCommits(parent, self);
+}
+
+function connectCommits(parent, self) {
+    console.log('connecting' + parent, ' ' + self);
+    const from = make('line');
+    config(from, {
+        x1: +parent.getAttribute('cx'),
+        y1: +parent.getAttribute('cy'),
+        x2: +self.getAttribute('cx'),
+        y2: +parent.getAttribute('cy'),
+        stroke: 'black',
+        'stroke-width': 2,
+    });
+    svg.appendChild(from);
+}
+
+function findByNodeId(commitId) {
+    return [...document.querySelectorAll('circle')].find(
+        e => e.getAttribute('node') === commitId.slice(0, 6),
+    );
+}
+
+function byUniqueBranch(b, i) {
+    return log.findIndex(e => b.branch === e.branch) === i;
+}
+
+function revisionSpread(b) {
     const revs = log
         .filter(commit => commit.branch === b.branch)
         .map(commit => commit.rev);
-    const top = (upperCommit - Math.max(...revs)) * COMMIT_SPAN;
-    const bottom = (upperCommit - Math.min(...revs)) * COMMIT_SPAN;
+    const top = COMMIT_SPAN + (upperCommit - Math.max(...revs)) * COMMIT_SPAN;
+    const bottom =
+        COMMIT_SPAN + (upperCommit - Math.min(...revs)) * COMMIT_SPAN;
     const branch = b.branch;
     return {
         top,
@@ -48,44 +78,35 @@ function constructBranch(branch) {}
 
 function createLine(branch, index) {
     const x = BRANCH_SPAN + index * BRANCH_SPAN;
-    const line = make('line');
-    config(line, {
-        x1: x,
-        y1: branch.top,
-        x2: x,
-        y2: branch.bottom,
-        'stroke-width': LINE_WIDTH,
-        stroke: 'red',
-        branch: branch.branch,
-    });
-    svg.appendChild(line);
+    const color = randomColor();
+    if (branch.top !== branch.bottom) {
+        const line = make('line');
+        config(line, {
+            x1: x,
+            y1: branch.top,
+            x2: x,
+            y2: branch.bottom,
+            'stroke-width': LINE_WIDTH,
+            stroke: color,
+            branch: branch.branch,
+        });
+        svg.appendChild(line);
+    }
     branches[branch.branch].forEach(commit => {
         const circle = make('circle');
         config(circle, {
             cx: x,
-            cy: (upperCommit - commit.rev) * COMMIT_SPAN,
+            cy: COMMIT_SPAN + (upperCommit - commit.rev) * COMMIT_SPAN,
             r: COMMIT_R,
-            fill: 'red',
+            fill: color,
+            node: commit.node.slice(0, 6),
+            branch: branch.branch,
         });
-        svg.appendChild(circle)
+        svg.appendChild(circle);
     });
-
 }
 
 function createLinkToParentBranch(branch) {}
-
-function createCommit(cx, cy, fill, commit) {
-    const circle = make('circle');
-    config(circle, {
-        cx,
-        cy,
-        fill,
-        r: COMMIT_R,
-        branch: commit.branch,
-        node: commit.node.slice(0, 6),
-    });
-    svg.appendChild(circle);
-}
 
 function config(element, props) {
     Object.keys(props).forEach(key =>
